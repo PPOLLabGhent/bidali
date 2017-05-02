8c279a18987eaaae79fa4f8e538b949146f6d644
Christophe Van Neste <christophe.vanneste@ugent.be>
2017-05-02 14:53:18 +0200

    before connecting to github ugent

--- seqanalysis.py
+++ seqanalysis.py
@@ -111,7 +111,91 @@ def literatureLinkSearch(term,referenceTerm='quadruplex'):
         return np.nan
     root = ET.fromstring(r.text)
     return [i.text for i in root.findall('IdList/Id')]
+
+# Signature enrichment test
+def calcSignature(counts,up,down=None,annotation=None,ax=None,xax=None,grouping=None,g4sig=False,
+                  medianLines=True,cortests=True,**kwargs):
+    """
+    - signature (up,down) should be provided as (a) pandas.Series
+    - grouping should be of the form (grouping_func,markers_dict) -> e.g. (lambda x: x.split('_')[0],{'SH-SY5Y':'s','IMR32':'o'})
+            grouping_func should provide as result all markers_dict keys
+    - xax if used should be pd.Series with index corresponding to counts.columns
+    """
+    import pandas as pd
+    if type(up) == str:
+        if not 'signatures' in dir(testSignature):
+            testSignature.signatures = pd.read_table('/home/christophe/Dropbiz/Lab/z_archive/Datasets/dnupGenesetsOfInterest.gmt',
+                                                     index_col=0,header=None)
+            testSignature.sigdescriptions = testSignature.signatures.pop(1)
+        up = testSignature.signatures.ix[up].dropna()
+    if annotation is not None:
+        up = annotation[annotation.gene_label.isin(up)].index
+    ranks = counts.rank()
+    rankSum = ranks[ranks.index.isin(up)].sum()
+    if g4sig:
+        rankSum = calcGlobalG4sig(ranks,ensembl,rank=False) if type(g4sig) == bool else g4sig
+    xax = xax.sort_values() if xax is not None else pd.Series(range(len(rankSum)),rankSum.index)
+    if ax:
+        rankRel = rankSum/rankSum.max()
+        if grouping and down is None:
+            for gname,grp in rankRel.groupby(grouping[0]):
+                ax.scatter([xax.ix[i] for i in grp.index],grp,c='r',s=30,marker=grouping[1][gname])
+        else: ax.scatter([xax.ix[i] for i in rankRel.index],rankRel,c='r',label='up ({})'.format(len(up)),**kwargs)
+        ax.set_xticks(xax)
+        ax.set_xticklabels(xax.index,rotation=-30,ha='left')
+        padding = (max(xax)-min(xax))*0.01
+        ax.set_xlim((min(xax)-padding,max(xax)+padding))
+        if medianLines:
+            ax.axvline(xax.median())
+            ax.axhline(rankRel.median())
+        if cortests:
+            from scipy.stats import spearmanr,pearsonr,fisher_exact
+            spearesults = spearmanr([xax.ix[i] for i in rankRel.index],rankRel)
+            ax.text(.99,.99,
+                    'Spearman: {:.2f} ({:.4f})\nPearson: {:.2f} ({:.4f})\nFisherET: {:.2f} ({:.4f})'.format(
+                        *spearesults,
+                        *pearsonr([xax.ix[i] for i in rankRel.index],rankRel),
+                        *fisher_exact([[sum((xax<xax.median())&(rankRel>rankRel.median())),
+                                        sum((xax>xax.median())&(rankRel>rankRel.median()))],
+                                       [sum((xax<xax.median())&(rankRel<rankRel.median())),
+                                        sum((xax>xax.median())&(rankRel<rankRel.median()))]],
+                                      alternative = 'less' if spearesults[0]<0 else 'greater')
+                    ),
+                    transform=ax.transAxes,va='top',ha='right')
+    if down is not None:
+        if type(down) == str:
+            down = testSignature.signatures.ix[down].dropna()
+        if annotation is not None:
+            down = annotation[annotation.gene_label.isin(down)].index
+        ranksDown = counts.rank(ascending=False)
+        rankDownSum = ranksDown[ranksDown.index.isin(down)].sum()
+        rankTotalSum = pd.DataFrame({'up':rankSum,'down':rankDownSum}).mean(axis=1)
+        if ax:
+            ax.scatter([xax.ix[i] for i in rankDownSum.index],rankDownSum/rankDownSum.max(),c='b',label='down ({})'.format(len(down)))
+            if grouping:
+                for gname,grp in (rankTotalSum/rankTotalSum.max()).groupby(grouping[0]):
+                    ax.scatter([xax.ix[i] for i in grp.index],grp,c='g',s=40,marker=grouping[1][gname])
+            else: ax.scatter([xax.ix[i] for i in rankTotalSum.index],rankTotalSum/rankTotalSum.max(),c='g',s=40,marker='s',label='mean')
+            ax.legend()
+        return rankTotalSum/rankTotalSum.max()
+    else:
+        return rankSum/rankSum.max()
+
+def calcGlobalG4sig(countRanks,geneG4annotation,colG4='G4s',rank=True):
+    """
+    If rank=True, assume counts were given and rank them
+    Scores like this -> sum(log(geneRank**G4s)) == sum(G4s*log(geneRank))
+    """
+    countRanks = countRanks[countRanks.index.isin(geneG4annotation.index)]
+    if rank: countRanks = countRanks.rank()
+    #countRanks.apply(lambda x: x.apply(lambda y,x=x: y**int(ensembl.ix[x.name].G4s)),axis=1).applymap(np.log).sum()
+    return countRanks.applymap(
+        np.log).apply(
+            lambda x: x.apply(
+                lambda y,x=x: int(geneG4annotation.ix[x.name][colG4])*y),axis=1).sum()    
     
+    
+# In main section below, some G4 'nifty' programming -> should be moved to dedicated G4 research script
 if __name__ == '__main__':
     genome = loadHumanGenome()
     import re
