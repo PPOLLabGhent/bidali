3853f1321bb408096bb243a8caa67f9ba85dcd12
Christophe Van Neste <christophe.vanneste@ugent.be>
2017-06-12 21:42:12 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name = 'lospy',
+      version = '0.1',
+      description = 'Lab-related occult and secretive python package',
+      url = 'https://github.ugent.be/cvneste/lospy',
+      author = 'Christophe Van Neste',
+      author_email = 'christophe.vanneste@ugent.be',
+      license = 'MIT',
+      packages = ['lospy'],
+      install_requires = [
+          'pylatex',
+      ],
+      zip_safe = False,
+      test_suite = 'nose.collector',
+      tests_require = ['nose']
+)
+
+#To install with symlink, so that changes are immediately available:
+#pip install -e . 
