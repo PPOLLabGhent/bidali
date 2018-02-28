f9fbdc9000f2b5e06c260c244745cde324fefcd6
Christophe Van Neste <christophe.vanneste@ugent.be>
2018-02-28 16:26:55 +0100

    LSD dealer import bug

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'bidali',
-      version = '0.1.0',
+      version = '0.1.1',
       description = 'Biological Data Analysis Library',
       url = 'https://github.com/beukueb/bidali',
       author = 'Christophe Van Neste',
