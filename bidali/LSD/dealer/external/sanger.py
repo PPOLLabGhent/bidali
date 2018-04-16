d7fc95113fc44f5228605a6d87535a0bd34c41eb
Christophe Van Neste <christophe.vanneste@ugent.be>
2018-04-16 15:09:45 +0200

    sanger

--- bidali/LSD/dealer/external/sanger.py
+++ bidali/LSD/dealer/external/sanger.py
@@ -12,4 +12,4 @@ def get_census():
 
     Locked source: https://cancer.sanger.ac.uk/cosmic/s3download?data=GRCh38%2Fcosmic%2Fv84%2Fcancer_gene_census.csv
     """
-    return pd.read_csv(os.path.join(LSD.processedDataStorage,'cancer_gene_census.csv',index_col=0)
+    return pd.read_csv(os.path.join(LSD.processedDataStorage,'cancer_gene_census.csv'),index_col=0)
