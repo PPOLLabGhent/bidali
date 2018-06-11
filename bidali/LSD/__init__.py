e74134907a24fd96bb4d9f6a881f9bf807687241
Christophe Van Neste <christophe.vanneste@ugent.be>
2018-06-11 10:26:03 +0200

    xarray dataset and saving as pandas table zipfolder

--- bidali/LSD/__init__.py
+++ bidali/LSD/__init__.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
-# LSD: Localised Structured Datasets
-# Module that preprocesses commonly used datasets
-# and makes them available for use in python.
+# -*- coding: utf-8 -*-
+"""LSD: Localised Structured Datasets
+Module that preprocesses commonly used datasets
+and makes them available for use in python.
+"""
 
 import gzip, pickle, time, os
 from zipfile import ZipFile
 from io import TextIOWrapper, StringIO
-import pandas as pd, numpy as np
+import pandas as pd, numpy as np, xarray as xr
 from os.path import expanduser, exists
 from collections import OrderedDict
 from contextlib import redirect_stdout, redirect_stderr
@@ -35,9 +36,10 @@ def listLSDatasets(subpackages=True):
                 print(*[i for i in sorted(dict(getmembers(mol)))
                         if i.startswith('get_')],sep='\n',end='\n\n')
 
-class Dataset:
-    """
-    A Dataset object is a collection of datasets, accessible as
+# Structured dataset classes
+## General
+class Dataset(object):
+    """A Dataset object is a collection of data tables, accessible as
     attributes from the Dataset object.
 
     to_R pushes the sub datasets to R making them available in the
@@ -49,8 +51,155 @@ class Dataset:
             self.__setattr__(kw,kwargs[kw])
 
     def generateRData(self):
-        pass
+        raise NotImplementedError
+
+class IntegratedDataset(object):
+    """IntegratedDataset checks and assures that for a set of samples, 
+    features and datatypes all data is internally cross-referencable and
+    then stores it as an xarray.
+    """
+    def __init__(self, xarray_kwargs = {}, **kwargs):
+        self.data = xr.Dataset(data_vars = kwargs, **xarray_kwargs)
+
+    def save(self,filename,csvsep=',',csvdec='.'):
+        """Save IntegratedDataset to disk
+
+        Iterates over the individual data variables and saves them
+        in a zipfolder
+
+        Args:
+            filename (str): Save location. Extension '.zip' will be added.
+        """
+        import zipfile, io
+        with zipfile.ZipFile('{}.zip'.format(filename), mode='w') as zdir:
+            for datatablename in self.data.data_vars:
+                b = io.StringIO()
+                self.data.data_vars[datatablename].to_pandas().to_csv(b,sep=csvsep,decimal=csvdec)
+                b.seek(0)
+                zdir.writestr('{}.csv'.format(datatablename),b.read())
+                #with zdir.open('{}.csv'.format(datatablename), mode='w') as zipf:
+                #    zipf.write(b.read().encode())
+                # Set touch time to current time
+                #zi = zdir.getinfo('{}.csv'.format(datatablename))
+                #zi.date_time = zipfile.time.localtime()[:6]
+                
+    @staticmethod
+    def load(filename):
+        """Load IntegratedDataset from disk zip folder.
+
+        Args:
+            filename (str): Save location. Extension '.zip' will be added.
+        """
+        raise NotImplementedError('serializing and loading not yet working')
+        return xr.open_dataset(filename)
+
+    # def save_netcdf(self, filename):
+    #     """Save IntegratedDataset to disk
+
+    #     Uses xr.Dataset netCDF method
+
+    #     Args:
+    #         filename (str): Save location. Suggested extension '.nc'.
+    #     """
+    #     raise NotImplementedError('Issues with netcdf output')
+    #     self.data.to_netcdf(filename)
+
+    # @staticmethod
+    # def load_from_netcdf(filename):
+    #     """Load IntegratedDataset from disk
+
+    #     Uses xr.Dataset netCDF method
+
+    #     Args:
+    #         filename (str): Filename of dataset to load.
+    #     """
+    #     raise NotImplementedError('serializing and loading not yet working')
+    #     return xr.open_dataset(filename)
     
+
+## Specific integrated datasets
+class CoexDataset(IntegratedDataset):
+    """IntegratedDataset that contains data on
+    copy number alteration status of genes (optionally
+    including the regions), and gene expression data.
+
+    *conureal* is optional, but if provided and *conugeal* is None,
+    *conugeal* is calculated based on *conureal* and the genes in
+    *expression*.
+
+    Args:
+        conugeal (pd.DataFrame): Copy number gene alterations, samples should be columns and genes rows.
+        expression (pd.DataFrame): Expression data, samples should be columns and genes rows.
+        metadata (pd.DataFrame): Annotation data should be columns and samples rows.
+            Recommended columns are `DOD` (bool, "dead of disease"), `POD` (bool, "progress of disease"), `overallSurvival` (int), `progressionFreeSurvival` (int)
+        metadataColMapping (dict): dict if metadata column names need to be changed to follow recommendations, e.g. {'survived':'DOD'}.
+        conureal (pd.DataFrame): Copy number region alterations. 
+            Required columns are `chrom`, `chromStart`, `chromEnd` and `samples`.
+        conurealColMapping (dict): dict if conureal column names need to be changed to follow requirements , e.g. {'chr':'chrom'}
+        filterUnknownExpressed (bool): If True, genes for which there is no expression data, will be filtered from conugeal.
+    """
+    def __init__(self,conugeal,expression,metadata,metadataColMapping={},conureal=None,conurealColMapping={},filterUnknownExpressed=True):
+        # Applying col mapping
+        if metadataColMapping: metadata = metadata.rename(metadataColMapping,axis=1)
+        if conurealColMapping: conureal = conureal.rename(conurealColMapping,axis=1)
+
+        # Checking data
+        import warnings
+        from pandas.api import types
+        typefns = {bool:types.is_bool_dtype,int:types.is_integer_dtype}
+        for n,t in (
+                ('DOD',bool),
+                ('POD',bool),
+                ('overallSurvival',int),
+                ('progressionFreeSurvival', int)
+        ):
+            if n in metadata and not typefns[t](metadata[n]):
+                warnings.warn('%s in metadata is not of correct type %s' % (n,t))
+            elif n not in metadata:
+                warnings.warn('%s not in metadata' % n)
+        
+        # If only regions is provided, and conugeal is None, calculate gene copy number values for all expression genes
+        if conureal and not conugeal:
+            conugeal = self.__class__.calculate_conugeal(conureal,expression.index)
+        self.conureal = conureal
+
+        # Filter genes for which there is no expression data
+        if filterUnknownExpressed:
+            conugeal = conugeal.filter(items=expression.index,axis=0)
+            
+        #Setting dimension names
+        conugeal.columns.name = expression.columns.name = metadata.index.name = 'sample'
+        conugeal.index.name = expression.index.name = 'gene'
+        metadata.columns.name = 'annotation'
+        if conureal:
+            conureal.columns.name = 'sample'
+            conureal.index.name = 'region'
+            
+        super().__init__(
+            conugeal=conugeal,
+            expression=expression,
+            metadata=metadata
+        )
+
+    @staticmethod
+    def calculate_conugeal(conureal,genes):
+        # Assign genes to regions
+        genannot = get_ensemblGeneannot()
+        segments['genes'] = segments.T.apply(
+            lambda x: {f.attributes['gene_name'][0] for f in genannot.region('{}:{}-{}'.format(
+                x.chromosome[3:],int(x.Start38),int(x.End38)),featuretype='gene')}
+        )
+        segments['nrGenes'] = segments.genes.apply(len)
+        del genannot#, get_genes
+        conugeal = {}
+        for sname,sample in segments.groupby('sample'):
+            conugeal[sname] = {}
+            for i in sample.T:
+                for g in sample.ix[i].genes: conugeal[sname][g] = sample.ix[i].value
+        conugeal = pd.DataFrame(conugeal)
+        return conugeal
+
+## Dataset repository
 class DatasetRepo:
     """
     Object containing:
@@ -447,22 +596,4 @@ def get_msigdb6():
     print('MSigDB {}'.format(genesetsCollections['version']))
     mdb = genesetsCollections['MSigDB']
     return mdb
-   
-if __name__ == '__main__':
-    import argparse
-    from rpy2.rinterface import RRuntimeError
-    from rpy2.robjects.packages import importr
-    import rpy2.robjects as ro
-    #Activate automatic pandas/r conversion
-    from rpy2.robjects import pandas2ri
-    pandas2ri.activate()
 
-    parser = argparse.ArgumentParser(description='Generate LSD dataset and store as RData.')
-    parser.add_argument('datasets', type=str, nargs='*', help='datasets to generate')
-    parser.add_argument('--list', action='store_true', help='list available datasets')
-
-    args = parser.parse_args()
-
-    if args.list:
-        listLSDatasets()
-    
